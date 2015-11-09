b51c86fe0e9eede0cb00bb189496b54dea5e293e
Christophe <Christophe@Chris>
2015-11-09 08:24:51 +0100

    Model manager : add and remove implementation

--- www/js/modelmanager.js
+++ www/js/modelmanager.js
@@ -3,6 +3,9 @@
 var ModelManager = function () {
 
     var objectsIndex = [];
+    var itemsById = [];
+    var usedIn = [];
+    var mmanager = this;
 
     var COLLECTION = "collection";
     var ENUMERATION = "enumeration";
@@ -39,6 +42,16 @@ var ModelManager = function () {
     }
 
 
+    this.getUsedInMapIndex = function (itemId, usedInMap) {
+        if (usedIn[itemId]) {
+            _.each(usedIn[itemId], function (uitem) {
+                usedInMap.push(uitem);
+                mmanager.getUsedInMapIndex(uitem, usedInMap);
+            });
+        }
+    };
+
+
     this.addItem = function (itemType, id, item) {
 
         var itemDescriptor = getDescriptor(itemType);
@@ -48,15 +61,36 @@ var ModelManager = function () {
             return null;
         }
 
-        var isValid = validateObject(itemType, id, item, item);
+        item.itemType = itemType;
+        item.itemId = id;
+
+        var itemsToRegister = [];
+        var isValid = validateObject(itemType, id, item, item, itemsToRegister);
+
+        _.each(itemsToRegister, function (itemId) {
+            if (!usedIn[itemId]) {
+                usedIn[itemId] = [];
+            }
+
+            usedIn[itemId].push(id);
+        });
 
         if (isValid) {
             if (itemDescriptor.indexable) {
                 setItem(itemType, id, item);
+                itemsById[id] = item;
             }
         } else {
             console.log("Modèle de l'objet non conforme : " + id);
         }
+    };
+
+    this.removeItem = function (itemId) {
+        var item = itemsById[itemId];
+        
+        delete (itemsById[itemId]);
+        delete (objectsIndex[item.itemType][itemId]);
+        
     };
 
     function isItemInIndex(objectType, id) {
@@ -112,7 +146,7 @@ var ModelManager = function () {
 
 
     function flatten(descriptorToFlatten, item, destObject) {
-        for(var descKey in descriptorToFlatten) {
+        for (var descKey in descriptorToFlatten) {
             var descProp = descriptorToFlatten[descKey];
 
             if (getPropertyType(descProp) === VARIABLE) {
@@ -135,7 +169,7 @@ var ModelManager = function () {
     }
 
 
-    function validateObject(itemType, id, item, baseitem) {
+    function validateObject(itemType, id, item, baseitem, itemsToRegister) {
         var itemDescriptor = getDescriptor(itemType);
 
         var flattenDesc = {};
@@ -156,7 +190,6 @@ var ModelManager = function () {
                 // on commence déjà par verifier si une propriété avec le même nom existe dans l'item
                 // sauf dans le cas des énumérations
                 if (pType !== ENUMERATION && !item[descriptorPropertyKey]) {
-                    //isItemValid = false;
                     console.log("propriété introuvable dans l'objet " + id + " : " + descriptorPropertyKey);
                     return false;
                 }
@@ -191,6 +224,8 @@ var ModelManager = function () {
                         if (!isItemInIndex(descriptorPropertyType, collectionItem[descriptorPropertyKey])) {
                             console.log("objet " + id + " : objet cible introuvable " + descriptorPropertyKey + " de type " + descriptorPropertyType);
                             return false;
+                        } else {
+                            itemsToRegister.push(collectionItem[descriptorPropertyKey]);
                         }
                     } else {
                         // on teste la conformité de la propriété
@@ -234,6 +269,10 @@ var ModelManager = function () {
     this.addItem("SpritesGroup", "gr1", {sprites: ["sp1", "sp2"]});
     this.addItem("Variable", "vr1", {type: "string", value: "test"});
     this.addItem("Condition", "cd1", {type: "variablecheck", variable: "vr1", variabletype: "string", operator: "===", value: "ok"});
+    this.addItem("Condition", "cd2", {type: "variablecheck", variable: "vr1", variabletype: "string", operator: "===", value: "ok"});
+
+    var uim = [];
+    this.getUsedInMapIndex("vr1", uim);
 
     return this;
 };
